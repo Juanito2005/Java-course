dcc756f2d454fa042b219cadd19a5f25fabf9a07
JuanCamilo <juanca.cuevas.05@outlook.com>
2024-09-19 12:55:56 +0200

    Variables excercise

---
+++ Variablesexcercise.java
@@ -0,0 +1,24 @@
+public class Variablesexcercise {
+
+    public static void main(String[] args) {
+        int z = 1234567890;
+        byte b = 123;
+        short averyshort = 12345;
+        long a = 1234567890123456789L;
+        float c = 3.1416f;
+        double p = 3.14;
+        boolean d = true;
+        char parentesis = ')';
+        String name = "Camilo";
+
+        System.out.println(z);
+        System.out.println(b);
+        System.out.println(averyshort);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(p);
+        System.out.println(d);
+        System.out.println(parentesis);
+        System.out.println("Arancha mira el trabajo de: " + name);
+    }
+}
