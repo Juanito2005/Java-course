92c1c9a793376fa553f4adf397bff8962a2b7eef
JuanCamilo <juanca.cuevas.05@outlook.com>
2024-09-19 14:06:57 +0200

    Cambio de variables

--- Variablesexcercise.java
+++ Variablesexcercise.java
@@ -2,7 +2,8 @@ public class Variablesexcercise {
 
     public static void main(String[] args) {
         int z = 1234567890;
-        byte b = 123;
+        int b = 234567890;
+        byte f = 123;
         short averyshort = 12345;
         long a = 1234567890123456789L;
         float c = 3.1416f;
@@ -19,6 +20,13 @@ public class Variablesexcercise {
         System.out.println(p);
         System.out.println(d);
         System.out.println(parentesis);
-        System.out.println("Arancha mira el trabajo de: " + name);
+        System.out.println("Aquí abajo estan las variables intercambiadas: " + name);
+
+        z = z + b;
+        b = z - b;
+        z = z -b;
+
+        System.out.println("x :" + z);
+        System.out.println("y :" + b);
     }
 }
